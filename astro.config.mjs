b53d9627232e2ffc3acc8574266f9ce0f03444a8
Jaydev Parmar <jydev@duck.com>
2026-02-28 16:09:57 +0530

    Fix staticness

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,7 +14,7 @@ export default defineConfig({
   adapter: vercel({
     imageService: true,
     isr: {
-      exclude: ["/apod", "/random"],
+      exclude: ["/apod", "/random", "/gallery"],
     },
   }),
 
